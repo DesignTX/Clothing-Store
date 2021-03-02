cce90232208dc76d1506e92588c6d5392aedff11
Tony Xu <Design.TXU@gmail.com>
2021-03-02 14:00:37 +1100

    added dynamic style component to menuItem

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 //Destructured by {title} which is equal to props.title
-const MenuItem = ({ title }) => (
-    <div className='menu-item'>
+const MenuItem = ({ title, imageUrl }) => (
+    //javascript template string, allows to dynamically create styles on component
+    <div style={{
+        backgroundImage: `url(${imageUrl})`
+    }} className='menu-item'>
         <div className='content'>
             <h1 className='title'>{title}</h1>
             <span className='subtitle'>SHOP NOW</span>
