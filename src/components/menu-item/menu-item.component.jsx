a03182a7fc19540cb7847b666aa9bb7ff64f4b25
Tony Xu <Design.TXU@gmail.com>
2021-03-03 14:29:32 +1100

    broken homepage into smaller components & finished styling menu-item components

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,12 +5,15 @@ import './menu-item.styles.scss';
 //Destructured by {title} which is equal to props.title
 const MenuItem = ({ title, imageUrl, size }) => (
     //react gives extra html mark up element called style, allows to dynamically create styles on component
-    <div style={{
-        backgroundImage: `url(${imageUrl})`
-    }} className={`${size} menu-item`}
-    >
+    <div className={`${size} menu-item`}>
+        <div 
+            className='background-image'  
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }} 
+        />
         <div className='content'>
-            <h1 className='title'>{title}</h1>
+            <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
