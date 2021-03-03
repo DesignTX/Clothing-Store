d8a2a715335c64e3659c2a9771419fbb4d3df3ab
Tony Xu <Design.TXU@gmail.com>
2021-03-03 14:08:07 +1100

    Added size value into menuItem and fixed css on large icons

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 //Destructured by {title} which is equal to props.title
-const MenuItem = ({ title, imageUrl }) => (
+const MenuItem = ({ title, imageUrl, size }) => (
     //react gives extra html mark up element called style, allows to dynamically create styles on component
     <div style={{
         backgroundImage: `url(${imageUrl})`
-    }} className='menu-item'>
+    }} className={`${size} menu-item`}
+    >
         <div className='content'>
             <h1 className='title'>{title}</h1>
             <span className='subtitle'>SHOP NOW</span>
