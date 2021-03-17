506d914db3b68de7e6d6d360f30a12d2b4d93dc9
Tony Xu <Design.TXU@gmail.com>
2021-03-18 10:18:43 +1100

    added switch, and moved routes inside of switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
@@ -10,12 +10,20 @@ const HatsPage = () => (
   </div>
 );
 
-// exact is a true or false property, if blank = true means path must be exactly whats parsed in for it to render, eg '/'
+// exact is a true or false property, 
+// if blank = true means path must be exactly whats parsed in for it to render, eg '/'
+
+// Switch finds a route inside that matches 
+// and renders only that route (will find the first match to render)
+// provides more control over our app
+
 function App() {
   return (
     <div>
+      <Switch>
       <Route exact path='/' component={HomePage} />
-      <Route path ='/hats' component={HatsPage} />
+      <Route path ='/hats' component={HatsPage} />        
+      </Switch>
     </div>
   );
 }
