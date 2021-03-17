1a0605c582f47035da88728921a5ed725581c27a
Tony Xu <Design.TXU@gmail.com>
2021-03-18 10:13:30 +1100

    Added routes into app.js to test function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
+import { Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+// exact is a true or false property, if blank = true means path must be exactly whats parsed in for it to render, eg '/'
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage} />
+      <Route path ='/hats' component={HatsPage} />
     </div>
   );
 }
